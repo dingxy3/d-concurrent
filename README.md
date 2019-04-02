fb23d16cbcc82fe90035186320967c4f25c823eb
dingxy3 <576789766@qq.com>
2019-04-02 11:25:09 +0800

    AQS5

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-java并发包
+### Java并发原理
+
+#### ReentrantLock源码剖析
+
+##### ReentrantLock代码结构
+```java
+
+```
---
+++ src/main/java/com/d/concurrent5/locks5/AbstractQueuedSynchronizer5.java
@@ -0,0 +1,13 @@
+package com.d.concurrent5.locks5;
+
+/**
+ * ============================
+ *
+ * @version [版本号, 2019/4/2]
+ * @Auther: dingxy
+ * @Description:
+ * @since [产品/模块版本]
+ * =============================
+ */
+public abstract class AbstractQueuedSynchronizer5 {
+}
