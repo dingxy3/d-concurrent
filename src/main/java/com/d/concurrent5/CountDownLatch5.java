08310e01c13dfa57b11af4981c9a7f433231907b
dingxy3 <576789766@qq.com>
2019-04-10 18:11:02 +0800

    countDown() await()

--- src/main/java/com/d/concurrent5/CountDownLatch5.java
+++ src/main/java/com/d/concurrent5/CountDownLatch5.java
@@ -1,5 +1,7 @@
 package com.d.concurrent5;
 
+import com.d.concurrent5.locks5.AbstractQueuedSynchronizer5;
+
 /**
  * ============================
  *
@@ -10,4 +12,39 @@ package com.d.concurrent5;
  * =============================
  */
 public class CountDownLatch5 {
+
+    private final static class  Sync extends AbstractQueuedSynchronizer5{
+
+        Sync(int count){
+           setState(count);
+        }
+       int getCount(){
+          return  getState();
+       }
+        protected int tryAcquireShared(int arg) {
+           return (getState()==0) ? 1 : -1 ;
+        }
+        /*释放锁通过compareAndSwap*/
+        protected boolean tryReleaseShared(int arg) {
+            for (;;) {
+                int c = getState();
+                if (c == 0)
+                    return false;
+                int nextc = c-1;
+                if (compareAndSetState(c, nextc))
+                    return nextc == 0;
+            }
+        }
+    }
+    private  Sync syn ;
+
+    public CountDownLatch5(int count){syn = new Sync(count);}
+
+    public void  countDown(){
+        syn.releaseShared(1);
+    }
+
+    public void await() throws InterruptedException {
+       syn.acquireSharedInterruptibly(1) ;
+    }
 }
