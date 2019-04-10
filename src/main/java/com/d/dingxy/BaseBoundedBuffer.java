8893e07d33e327d93d86d6c6ec7d551d55b8541c
dingxy3 <576789766@qq.com>
2019-04-10 22:47:04 +0800

    条件谓词

--- src/main/java/com/d/dingxy/BaseBoundedBuffer.java
+++ src/main/java/com/d/dingxy/BaseBoundedBuffer.java
@@ -19,11 +19,21 @@ public abstract class BaseBoundedBuffer<V> {
     }
 
     protected   synchronized  final V doTake(){
+        V v = buff[head];
 
+        return v ;
     }
 
     protected  synchronized final void  doPut(){
 
+    }
+
+    private synchronized final boolean isFull(){
+
+        return buff.length == count ;
+    }
+    private synchronized final boolean isEmpty(){
+        return  count == 0 ;
     }
 
 }
