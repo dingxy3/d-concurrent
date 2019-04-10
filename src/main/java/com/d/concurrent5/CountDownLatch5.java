af317809c03e26946feb7424651571babe75426e
dingxy3 <576789766@qq.com>
2019-04-10 17:28:30 +0800

    CountDownLatch5

---
+++ src/main/java/com/d/concurrent5/CountDownLatch5.java
@@ -0,0 +1,13 @@
+package com.d.concurrent5;
+
+/**
+ * ============================
+ *
+ * @version [版本号, 2019/4/10]
+ * @Auther: dingxy
+ * @Description:
+ * @since [产品/模块版本]
+ * =============================
+ */
+public class CountDownLatch5 {
+}
