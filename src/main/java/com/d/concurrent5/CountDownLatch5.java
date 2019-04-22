a90e58826b475246311045f374315a58ee01b812
dingxy3 <576789766@qq.com>
2019-04-22 22:53:25 +0800

    countDownLatch

--- src/main/java/com/d/concurrent5/CountDownLatch5.java
+++ src/main/java/com/d/concurrent5/CountDownLatch5.java
@@ -7,7 +7,7 @@ import com.d.concurrent5.locks5.AbstractQueuedSynchronizer5;
  *
  * @version [版本号, 2019/4/10]
  * @Auther: dingxy
- * @Description:
+ * @Description:线程汇聚，阻塞当前主线程，让其他线程先执行完。共享锁
  * @since [产品/模块版本]
  * =============================
  */
@@ -15,16 +15,28 @@ public class CountDownLatch5 {
 
     private  Sync syn ;
 
+    /**
+     * 构造方法初始化CountDownLatch
+     * @param count
+     */
     public CountDownLatch5(int count){syn = new Sync(count);}
 
     private final static class  Sync extends AbstractQueuedSynchronizer5{
 
 
+        /**
+         * AQS的state作为初始化计数器，即线程的数量
+         * @param count
+         */
         Sync(int count){
            setState(count);
         }
 
-       int getCount(){
+        /**
+         * 获得还剩余的线程数量
+         * @return
+         */
+        int getCount(){
           return  getState();
        }
 
@@ -37,13 +49,23 @@ public class CountDownLatch5 {
         /*释放锁通过compareAndSwap*/
         @Override
         protected boolean tryReleaseShared(int arg) {
+            //循环进行cas，直到当前线程成功完成cas使计数值（状态值state）减一并更新到state
             for (;;) {
+                //获得当前计数器的数目
                 int c = getState();
-                if (c == 0) {
+
+                //如果计数器为0
+                if (c == 0)
+                {
+                    //返回false
                     return false;
                 }
+
                 int nextc = c-1;
-                if (compareAndSetState(c, nextc)) {
+
+                //CAS设置计数值减一
+                if (compareAndSetState(c, nextc))
+                {
                     return nextc == 0;
                 }
             }
