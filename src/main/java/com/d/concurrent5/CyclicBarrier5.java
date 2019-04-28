1e5a64934f0d7f0077a6c0418dbf6458694462dc
dingxy3 <576789766@qq.com>
2019-04-28 13:40:35 +0800

    使用场景

--- src/main/java/com/d/concurrent5/CyclicBarrier5.java
+++ src/main/java/com/d/concurrent5/CyclicBarrier5.java
@@ -5,7 +5,7 @@ package com.d.concurrent5;
  *
  * @version [版本号, 2019/4/12]
  * @Auther: dingxy
- * @Description:线程栅栏（线程屏障）
+ * @Description:线程栅栏（线程屏障）人满发车
  * @since [产品/模块版本]
  * =============================
  */
